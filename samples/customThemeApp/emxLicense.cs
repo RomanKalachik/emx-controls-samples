2c7a5c184c98a4476c291420766384ccc07d6762
Kalachik Roman <romankalachik@eremex.ru>
2026-01-28 14:39:19 +0300

    added files

---
+++ samples/customThemeApp/emxLicense.cs
@@ -0,0 +1,16 @@
+// This file is auto-generated. Please do not change it.
+using System.Runtime.CompilerServices;
+
+using Eremex.AvaloniaUI.Controls.License;
+
+namespace customTheme;
+public class LicenseProvider
+{
+#pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+	[ModuleInitializer]
+#pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
+    public static void RegisterLicense()
+	{
+        ControlsLicenseManager.SetRuntimeLicenseOwner(new LicenseProvider(),"RomanKalachik", "000003E7", "0D 3C B9 58 FA 18 28 54 81 F4 E1 2F 70 86 76 2F F0 85 9D 8C EE 8F B3 89 1E 06 99 17 E6 CC CE 57 FD 0B 0A FA CF 51 17 85", "A5 3D 60 97 5C D5 24 F1 D3 EB 24 66 CE 39 FA 1A 7A 1D 47 F9 D6 F7 44 37 19 8E 1C 01 24 1F A1 70 8F 90 CD B6 7D 57 05 AC");
+	}
+}
